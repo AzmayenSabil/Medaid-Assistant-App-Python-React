89e7fdb780b5768a21678350565a9225c2b9ec4f
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-04-09 00:48:57 +0600

    updated

--- medical_questionnaire.py
+++ medical_questionnaire.py
@@ -1,18 +1,10 @@
 import json
-import tkinter as tk
-from tkinter import simpledialog, messagebox
 from getDomain import get_domain
 
 # Load the question sets from a JSON file
 with open('questions.json') as f:
     questions = json.load(f)
 
-# Define the domains
-domains = {
-    'heart': 'cardiology',
-    'stomach': 'gastroenterology',
-    'skin': 'dermatology'
-}
 
 # function to add to JSON
 def write_json(new_data, filename='results.json'):
@@ -25,88 +17,87 @@ def write_json(new_data, filename='results.json'):
         file.seek(0)
         # convert back to json.
         json.dump(file_data, file, indent=4)
-        clear_screen()
+
 
 # function to ask questions and store responses
-def ask_questions(question_set, chief_complaints):
+def ask_questions(complaint, domain):
     responses = {}
-    domain = determine_domain(chief_complaints)
-    if domain is None:
-        return None
+    question_set = questions[domain]
     for question in question_set:
-        response = tk.simpledialog.askstring("Question", question, parent=window)
-        if response is None:
+        response = input(question + "\n")
+        if response.strip() == '':
             break
         responses[question] = response
-        domain = determine_domain(chief_complaints + list(responses.values()))
-        if domain is None:
-            break
-    return responses, domain
+    return responses
 
 
 # function to determine domain based on chief complaints
-def determine_domain(chief_complaints):
-    domain = get_domain(chief_complaints)
-    if domain is None:
-        tk.messagebox.showerror("Error", "Sorry, we do not have any questions related to your chief complaint.")
+def determine_domain(chief_complaints, responses, domain):
+    if domain is not None:
+        return domain
+    new_domain = get_domain(chief_complaints + list(responses.values()))
+    if new_domain is None:
+        print("Sorry, we do not have any questions related to your chief complaint.")
         return None
     else:
-        return domain
+        return new_domain
 
 
+# function to collect user information and complaints
 def collect_user_info_and_complaints():
     user_info = {}
-    user_info['age'] = tk.simpledialog.askstring("Age", "What is your age?", parent=window)
-    user_info['gender'] = tk.simpledialog.askstring("Gender", "What is your gender?", parent=window)
-    user_info['medical_history'] = tk.simpledialog.askstring("Medical History", "Do you have any medical history?",
-                                                             parent=window)
+    user_info['age'] = input("What is your age?\n")
+    user_info['gender'] = input("What is your gender?\n")
+    user_info['medical_history'] = input("Do you have any medical history?\n")
 
     chief_complaints = []
 
-    def add_complaint():
-        complaint = complaint_entry.get()
-        if complaint != '':
-            chief_complaints.append(complaint)
-            complaint_entry.delete(0, tk.END)
-            complaints_listbox.insert(tk.END, complaint)
-
-    def submit_complaints():
-        if chief_complaints:
-            question_set = questions[determine_domain(chief_complaints)]
-            responses, domain = ask_questions(question_set, chief_complaints)
-            if domain is not None:
-                result = {'user_info': user_info, 'chief_complaints': chief_complaints, 'responses': responses,
-                          'domain': domain}
-                write_json(result)
-                tk.messagebox.showinfo("Success", "Your information has been saved.")
-                # window.destroy()
+    while True:
+        complaint = input("Enter your chief complaint (press Enter to finish):\n")
+        if complaint.strip() == '':
+            break
+        chief_complaints.append(complaint)
+        domain = determine_domain(chief_complaints, {}, None)
+        if domain is not None:
+            responses = ask_questions(complaint, domain)
+            result = {'user_info': user_info, 'chief_complaint': complaint, 'responses': responses, 'domain': domain}
+            write_json(result)
+            print("Your information has been saved.")
         else:
-            tk.messagebox.showerror("Error", "Please enter at least one chief complaint.")
+            print("Sorry, we do not have any questions related to your chief complaint.")
 
-    complaint_label = tk.Label(window, text="Enter your chief complaints:")
-    complaint_entry = tk.Entry(window)
-    add_button = tk.Button(window, text="Add", command=add_complaint)
-    complaints_listbox = tk.Listbox(window, height=5)
-    submit_button = tk.Button(window, text="Submit", command=submit_complaints)
+    if chief_complaints:
+        print("Thank you for providing your information.")
+        print("Please answer the following questions to help us improve our system.")
+        feedback = input("Was the data collection process easy to understand and complete? (yes/no)\n")
+        if feedback.lower() == 'yes':
+            print("We're glad to hear that. Thank you for your feedback!")
+        else:
+            print("We're sorry to hear that. Please let us know how we can improve the process.")
+    else:
+        print("Please enter at least one chief complaint.")
 
-    complaint_label.pack()
-    complaint_entry.pack()
-    add_button.pack()
-    complaints_listbox.pack()
-    submit_button.pack()
 
-
-# create a tkinter window
-window = tk.Tk()
-window.geometry("500x500")
-window.title("Patient Data Collection")
-
-button = tk.Button(text="Start Data Collection", command=collect_user_info_and_complaints)
-button.pack(pady=20)
 def clear_screen():
-    for widget in window.winfo_children():
-        widget.destroy()
-    button = tk.Button(text="Start Data Collection", command=collect_user_info_and_complaints)
-    button.pack(pady=20)
+    print("\n" * 8)
 
-window.mainloop()
+
+def main():
+    while True:
+        print("\nWelcome to Patient Data Collection!")
+        print("Please choose an option:")
+        print("1. Start Data Collection")
+        print("2. Exit")
+        choice = input()
+        if choice == '1':
+            clear_screen()
+            collect_user_info_and_complaints()
+        elif choice == '2':
+            print("Thank you for using Patient Data Collection!")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+if __name__ == "__main__":
+    main()
