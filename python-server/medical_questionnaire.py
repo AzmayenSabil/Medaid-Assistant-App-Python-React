8aa9722fbd759c8a7b05995c577de826fbbfc945
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-11-02 00:26:14 +0600

    worked on python backend

--- python-server/medical_questionnaire.py
+++ python-server/medical_questionnaire.py
@@ -1,103 +1,32 @@
+from flask import Flask, request, jsonify
 import json
-from getDomain import get_domain
 
-# Load the question sets from a JSON file
-with open('questions.json') as f:
-    questions = json.load(f)
+app = Flask(__name__)
 
+# Read the JSON data from the file
+with open('questions.json', 'r') as json_file:
+    data = json.load(json_file)
 
-# function to add to JSON
-def write_json(new_data, filename='results.json'):
-    with open(filename, 'r+') as file:
-        # First we load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
-        file_data["patient_details"].append(new_data)
-        # Sets file's current position at offset.
-        file.seek(0)
-        # convert back to json.
-        json.dump(file_data, file, indent=4)
+print("Loaded Data: ", data["specific_questions"][0])
 
+@app.route('/get_questions', methods=['POST'])
+def get_questions_route():
+    request_data = request.get_json()
+    chief_complaint = request_data.get('chief_complaint')
+    matched_question_set = None
 
-# function to ask questions and store responses
-def ask_questions(complaint, domain):
-    responses = {}
-    question_set = questions[domain]
-    for question in question_set:
-        response = input(question + "\n")
-        if response.strip() == '':
-            break
-        responses[question] = response
-    return responses
+    if chief_complaint:
+        for question_set in data["specific_questions"]:
+            if question_set["symptom"] == chief_complaint:
+                matched_question_set = question_set
+                break
 
-
-# function to determine domain based on chief complaints
-def determine_domain(chief_complaints, responses, domain):
-    if domain is not None:
-        return domain
-    new_domain = get_domain(chief_complaints + list(responses.values()))
-    if new_domain is None:
-        print("Sorry, we do not have any questions related to your chief complaint.")
-        return None
+    if matched_question_set:
+        questions = matched_question_set["questions"]
     else:
-        return new_domain
+        questions = []
 
+    return jsonify({"questions": questions})
 
-# function to collect user information and complaints
-def collect_user_info_and_complaints():
-    user_info = {}
-    user_info['age'] = input("What is your age?\n")
-    user_info['gender'] = input("What is your gender?\n")
-    user_info['medical_history'] = input("Do you have any medical history?\n")
-
-    chief_complaints = []
-
-    while True:
-        complaint = input("Enter your chief complaint (press Enter to finish):\n")
-        if complaint.strip() == '':
-            break
-        chief_complaints.append(complaint)
-        domain = determine_domain(chief_complaints, {}, None)
-        if domain is not None:
-            responses = ask_questions(complaint, domain)
-            result = {'user_info': user_info, 'chief_complaint': complaint, 'responses': responses, 'domain': domain}
-            write_json(result)
-            print("Your information has been saved.")
-        else:
-            print("Sorry, we do not have any questions related to your chief complaint.")
-
-    if chief_complaints:
-        print("Thank you for providing your information.")
-        print("Please answer the following questions to help us improve our system.")
-        feedback = input("Was the data collection process easy to understand and complete? (yes/no)\n")
-        if feedback.lower() == 'yes':
-            print("We're glad to hear that. Thank you for your feedback!")
-        else:
-            print("We're sorry to hear that. Please let us know how we can improve the process.")
-    else:
-        print("Please enter at least one chief complaint.")
-
-
-def clear_screen():
-    print("\n" * 8)
-
-
-def main():
-    while True:
-        print("\nWelcome to Patient Data Collection!")
-        print("Please choose an option:")
-        print("1. Start Data Collection")
-        print("2. Exit")
-        choice = input()
-        if choice == '1':
-            clear_screen()
-            collect_user_info_and_complaints()
-        elif choice == '2':
-            print("Thank you for using Patient Data Collection!")
-            break
-        else:
-            print("Invalid choice, please try again.")
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
