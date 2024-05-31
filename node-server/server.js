0bd7ce15eb413c516a31217d18048260492e756a
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2024-06-01 04:01:47 +0600

    Update server.js

--- node-server/server.js
+++ node-server/server.js
@@ -7,9 +7,25 @@ const symptomRoutes = require('./routes/symptomsRoutes');
 
 const app = express();
 
+const allowedOrigins = [
+  "https://medaid-assistant-app-client-2le6jqjul-azmayensabils-projects.vercel.app",
+  "http://localhost:5000",
+];
+
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 // Routes
 app.use("/questions", questionRoutes);
