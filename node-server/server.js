755b05c523b481dc3ee34ed8ae8032bd0d813ddd
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2024-06-01 04:07:59 +0600

    Update server.js

--- node-server/server.js
+++ node-server/server.js
@@ -14,18 +14,18 @@ const allowedOrigins = [
 
 // Middleware
 app.use(bodyParser.json());
-// app.use(cors());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
 
 // Routes
 app.use("/questions", questionRoutes);
